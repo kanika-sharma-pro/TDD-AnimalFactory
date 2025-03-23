62b26de07a429d933ff310f131a6c41e0a1f7992
Kanika <kanika@macbookpro.lan>
2025-03-23 12:32:29 -0400

    trying to make animal factory test but there is an error

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,14 +1,33 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+@Test
+    public void testCreateDog() {
+    Date birthDate = new Date();
+    Dog dog = AnimalFactory.createDog("Milo", birthDate);
+    Assert.assertEquals("Milo", dog.getName());
 
+}
 
+    @Test
+    public void testCreateCat() {
+        Date birthDate = new Date();
+        Cat cat = AnimalFactory.createCat("Zula", birthDate);
+        Assert.assertEquals("Zula", Cat.getCatName());
 
+    }
 
 
 
